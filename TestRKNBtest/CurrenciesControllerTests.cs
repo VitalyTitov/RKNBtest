11b36fff8b972259a8d9febbee31838a798c2d6b
Vitaly Gorelenko <titov.vitaliy2014@yandex.ru>
2021-08-29 17:20:03 +0300

    Currency fix

---
+++ TestRKNBtest/CurrenciesControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RKNBtest.Controllers;
+using System.Threading.Tasks;
+
+namespace TestRKNBtest
+{
+    public class CurrenciesControllerTests
+    {
+        
+        public async Task GetTest()
+        {
+            // Arrange
+            CurrenciesController controller= new CurrenciesController();
+            // Act
+            ViewResult result = (ViewResult)await controller.Get().ConfigureAwait(false);
+            // Assert
+            Assert.IsNotNull(result);
+        }
+      
+    }
+}
---
+++ TestRKNBtest/CurrencyControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace TestRKNBtest
+{
+    class CurrencyControllerTest
+    {
+        [TestMethod]
+        public void Currency_Test()
+        {
+
+        }
+        
+        [TestMethod]
+        public void GetValute_Test([FromForm] string idvalute)
+        {
+
+        }
+
+        [TestMethod]
+        public void GetValuteinfo_Test(string idvalute)
+        {
+
+        }
+    }
+}
